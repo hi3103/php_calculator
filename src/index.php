0b7f51c2b9d6730f045503b7ac83ab2013342e9c
hi3103 <info@hi3103.net>
2023-10-24 19:19:27 +0900

    ヘッダー／フッター要素の追加、軽微なスタイル調整（全体中央寄せ＋背景色変更）

--- src/index.php
+++ src/index.php
@@ -10,38 +10,50 @@
 </head>
 <body>
 
-<div id="calculator">
+<div class="wrapper">
+	<header id="header">
+		<h1>PHP Calculator</h1>
+	</header>
 
-	<div id="notice"></div>
-	<div id="display">0</div>
+	<main id="main">
+		<div id="calculator">
 
-	<!-- 数字のボタン -->
-	<div id="number-buttons">
-		<button class="btn btn-number" data-value="1">1</button>
-		<button class="btn btn-number" data-value="2">2</button>
-		<button class="btn btn-number" data-value="3">3</button>
-		<button class="btn btn-number" data-value="4">4</button>
-		<button class="btn btn-number" data-value="5">5</button>
-		<button class="btn btn-number" data-value="6">6</button>
-		<button class="btn btn-number" data-value="7">7</button>
-		<button class="btn btn-number" data-value="8">8</button>
-		<button class="btn btn-number" data-value="9">9</button>
-		<button class="btn btn-number" data-value="0">0</button>
-	</div>
+			<div id="notice"></div>
+			<div id="display">0</div>
 
-	<!-- 演算用のボタン -->
-	<div id="formula-buttons">
-		<button class="btn btn-formula" data-value="/">÷</button>
-		<button class="btn btn-formula" data-value="*">×</button>
-		<button class="btn btn-formula" data-value="-">-</button>
-		<button class="btn btn-formula" data-value="+">+</button>
-		<button class="btn btn-formula" data-value="=">=</button>
-	</div>
+			<!-- 数字のボタン -->
+			<div id="number-buttons">
+				<button class="btn btn-number" data-value="1">1</button>
+				<button class="btn btn-number" data-value="2">2</button>
+				<button class="btn btn-number" data-value="3">3</button>
+				<button class="btn btn-number" data-value="4">4</button>
+				<button class="btn btn-number" data-value="5">5</button>
+				<button class="btn btn-number" data-value="6">6</button>
+				<button class="btn btn-number" data-value="7">7</button>
+				<button class="btn btn-number" data-value="8">8</button>
+				<button class="btn btn-number" data-value="9">9</button>
+				<button class="btn btn-number" data-value="0">0</button>
+			</div>
 
-	<!-- その他操作用のボタン -->
-	<div id="function-buttons">
-		<button class="btn btn-function" data-value="AC">AC</button>
-	</div>
+			<!-- 演算用のボタン -->
+			<div id="formula-buttons">
+				<button class="btn btn-formula" data-value="/">÷</button>
+				<button class="btn btn-formula" data-value="*">×</button>
+				<button class="btn btn-formula" data-value="-">-</button>
+				<button class="btn btn-formula" data-value="+">+</button>
+				<button class="btn btn-formula" data-value="=">=</button>
+			</div>
+
+			<!-- その他操作用のボタン -->
+			<div id="function-buttons">
+				<button class="btn btn-function" data-value="AC">AC</button>
+			</div>
+		</div>
+	</main>
+
+	<footer id="footer">
+		<p>Made by <a href="https://hi3103.net" target="_blank">hi3103.net</a></p>
+	</footer>
 </div>
 
 </body>
