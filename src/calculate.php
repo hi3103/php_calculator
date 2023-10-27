b1373a8cd6fbca2e1f00c5ab4b67a0b78d743734
hi3103 <info@hi3103.net>
2023-10-27 21:36:03 +0900

    [修正] PHP：計算処理を eval から自作関数に変更

--- src/calculate.php
+++ src/calculate.php
@@ -10,8 +10,45 @@ if (empty($expression)) {
 
 // 計算結果を返却
 try {
-	$result = eval("return {$expression};");
-	echo $result;
+	// 計算処理を関数として定義
+	function calculateSimpleExpression($expression){
+		$result = null;
+
+		// 計算式を分解して四則演算を実行
+		if (strpos($expression,'+')!==false) {
+			list($a,$b) = explode('+',$expression);
+			$result = $a+$b;
+		}
+		if (strpos($expression,'-')!==false) {
+			list($a,$b) = explode('-',$expression);
+			$result = $a-$b;
+		}
+		if (strpos($expression,'*')!==false) {
+			list($a,$b) = explode('*',$expression);
+			$result = $a*$b;
+		}
+		if (strpos($expression,'/')!==false) {
+			list($a,$b) = explode('/',$expression);
+			if ($b==0) {
+				throw new Exception("0で割ることはできません。");
+			}
+			$result = $a/$b;
+		}
+
+		return $result;
+	}
+
+	// 受け取った計算式に対して実行
+	$result = calculateSimpleExpression($expression);
+
+	// 計算結果を返す
+	if($result!==null){
+		echo $result;
+	}else{
+		echo "Error";
+	}
+
+// 予期せぬエラーが発生した場合の処理
 } catch (Throwable $e) {
 	echo "Error";
 }
